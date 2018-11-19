21e4043429d1274d05c7ed63e47f218fb8d2a0ef
Saket Roy <theonlyroy@Sakets-MacBook-Pro.local>
2018-11-19 14:37:31 -0600

    Add Article Headlines to Module

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/', function(req, res) {
 });
     request ({
     method: 'GET',
-    url: `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=wildfires`,
+    url: `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=hawley`,
     qs: {
         'api-key': "f0a4f818f6884462aba9a8b7f18c3c42"
       },
@@ -25,13 +25,12 @@ app.get('/', function(req, res) {
     function(error,response,body){
         let arr = body.response.docs;
         arr.forEach(function(article){
-            console.log(article.headline.main);
-            articleModule.addArticleHeadlines(article.headline.main);
+            articleModule.addArticleHeadlines(article.headline.main);  
         });
-        //console.log(body.response.docs[0].headline.main);
+        console.log(articleModule.getArticleHeadlines());
 });
 
-console.log(articleModule.getArticleHeadlines());
+
 const server = app.listen(3000, function() {
 	console.log(`Server started on port ${server.address().port}`);
 });
