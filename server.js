f41616faa15ef47bd5e081cc55581ce7ffbdda7a
Erica Gitlin <ericagitlin@Ericas-MBP-010013034048.MissouriState.edu>
2018-11-19 19:03:45 -0600

    changes

--- server.js
+++ server.js
@@ -7,15 +7,16 @@ const express = require('express'),
 app.use(express.static('resources'));
 
 app.set('view engine', 'pug');
-app.set('views', 'static/views');
+app.set('views', 'views');
 
 app.get('/', function(req, res) {
     let result = req.query.q;
     res.render('web');
-});
+}
+);
     request ({
     method: 'GET',
-    url: `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${result}`,
+    url: `https://api.nytimes.com/svc/search/v2/articlesearch.json`,
     qs: {
         'api-key': "f0a4f818f6884462aba9a8b7f18c3c42"
       },
