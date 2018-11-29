197ae242d67da50f21015c48ee6f120dcf6cc126
Erica Gitlin <ericagitlin@Ericas-MBP-010013034048.MissouriState.edu>
2018-11-28 19:59:30 -0600

    login is working

--- server.js
+++ server.js
@@ -3,9 +3,60 @@
 const express = require('express'),
     app = express(),
     request = require('request'),
-    articleModule = require('./modules/storage.js');
+    articleModule = require('./modules/storage.js'),
+    passport = require("passport"), //used for logins and sessions
+	cookieParser = require("cookie-parser"),//used for logins and sessions
+	expressSession = require("express-session"),//used for logins and sessions
+	LocalStrategy = require("passport-local").Strategy, //used for logins and sessions
+	objL = {          //obj used to fill out the login template to make it  the login page 
+		title: "Login",
+		bottomMessage: "Don't have an account? Click the register tab!",
+		homeTab: "login",
+		secondaryTab: "Register", 
+		submitButton: "Login",
+		homeTabPath: "/",
+		secondaryTabPath: "/home",
+		httpAction: "/login_attempt"
+	};
 
 app.use(express.static('resources'));
+app.use(cookieParser());
+app.use(expressSession({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy({usernameField: "username", passwordField: "password"},
+        function(username,password,done){
+            if(username !== password){
+                return done(null, false, {
+                        message: "Sorry, username or password not found"
+                });
+            }
+            return done(null, {
+                user: username
+            });
+        })
+);
+passport.serializeUser(function(user,done){
+    done(null,user.user);
+});
+passport.deserializeUser(function(id,done){
+    done(null,{
+        user:id,
+    });
+});
+
+let ensureAuthenticated = function(req, res, next) {
+    if(req.isAuthenticated()) {
+        return next();
+    }
+    else {
+        res.redirect("/");
+    }
+}
 
 app.set('view engine', 'pug');
 app.set('views', 'views');
@@ -17,12 +68,24 @@ app.use(
 	})
 );
 
+app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+})
+
 app.get('/', function(req, res) {
-    res.render('web',{
-            articles: articleModule.getArticleHeadlines()
-    });
+    res.render('login', objL);
 
 });
+
+app.post("/login", passport.authenticate("local",{
+	failureRedirect: "/",
+	successRedirect: "/home"
+}));
+
+app.get('/home', function(req, res) {
+    res.send("Thanks! you've been logged in");
+})
 
 app.get('/search', function(req, res) {
     res.render('search', {
