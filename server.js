48f9e062670370f836f7dd97748574fb95a3ff16
Saket Roy <theonlyroy@Sakets-MBP.MissouriState.edu>
2018-11-28 20:07:25 -0600

    Update server.js

--- server.js
+++ server.js
@@ -84,50 +84,18 @@ app.post("/login", passport.authenticate("local",{
 }));
 
 app.get('/home', function(req, res) {
-    res.send("Thanks! you've been logged in");
-})
-
-app.get('/search', function(req, res) {
-    res.render('search', {
+    //res.send("Thanks! you've been logged in");
+    res.render('web',{
         articles: articleModule.getArticleHeadlines()
-    });
-    let result = req.query.term;
-    request.get ({
-    method: 'GET',
-    url: `https://api.nytimes.com/svc/search/v2/articlesearch.json?term=${result}`,
-    qs: {
-        'api-key': "f0a4f818f6884462aba9a8b7f18c3c42",
-        'q': `${result}`
-      },
-    json: true},
-
-    function(error,response,body){
-        let arr = body.response.docs;
-        arr.forEach(function(article){
-            articleModule.addArticleHeadlines(article.headline.main);  
-        });
-
-    });
-
-    const articleList = articleModule.getArticleHeadlines();
-    res.json(articleList);
-
-    articleModule.clearArticleHeadlines();
 });
 
-app.get('/articleList', function(req, res) {
-    const list = articleModule.getArticleHeadlines();
-    res.json(list);
 })
 
 app.post('/search', function(req, res) {
-    res.render('search', {
-        articles: articleModule.getArticleHeadlines()
-    });
-    let result = req.query.term;
-    request.get ({
-    method: 'POST',
-    url: `https://api.nytimes.com/svc/search/v2/articlesearch.json?term=${result}`,
+    let result = req.body.article || 'apple';
+    request ({
+    method: 'GET',
+    url: `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=`,
     qs: {
         'api-key': "f0a4f818f6884462aba9a8b7f18c3c42",
         'q': `${result}`
@@ -139,19 +107,11 @@ app.post('/search', function(req, res) {
         arr.forEach(function(article){
             articleModule.addArticleHeadlines(article.headline.main);  
         });
-
-    });
-
-    const articleList = articleModule.getArticleHeadlines();
-    res.json(articleList);
-
-    articleModule.clearArticleHeadlines();
+        const articleList = articleModule.getArticleHeadlines();
+        res.json(articleList);
+        articleModule.clearArticleHeadlines();  
+    }); 
 });
-
-app.get('/articleList', function(req, res) {
-    const list = articleModule.getArticleHeadlines();
-    res.json(list);
-})
 
 
 const server = app.listen(3000, function() {
